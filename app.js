d78b696f6f9abad59d88b6c9b4eb7bf49a1034c9
Laurenda Edorh <laurenda98@gmail.com>
2024-11-25 12:39:56 +0100

    ajout des fichiers

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let button =document.getElementById("button");
-const changePokemon =()=>
+const changePokemon = async()=>
 {
     let randomNumber = Math.ceil(Math.random()*151) +1;
-}
+    let requestString =`https://pokeapi.co/api/v2/{randomNumber}`;
+    let data = await fetch(requestString);
+    console.log(data);
+    
+};
 button.addEventListener("click", changePokemon);
